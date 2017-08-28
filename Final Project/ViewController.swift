8cc07a6e13f57ec145bc19cc5fa47d36f7ba3716
Aanya Jhaveri <aanya@uchicago.edu>
2017-08-28 14:51:48 -0500

    sorted array of existing EKEvents

--- Final Project/ViewController.swift
+++ Final Project/ViewController.swift
@@ -11,49 +11,50 @@ import EventKit
 
 class ViewController: UIViewController {
     
-    let eventStore = EKEventStore()
-    var calendar: [EKCalendar]?
-    var conflict: [EKEvent]?
+    let eventStore = EKEventStore() //event store
+    var conflictArray: [EKEvent]? //array of events
+    var existingCalendarArray: [EKCalendar]? //array of existing calendars
+    var calendarsForEvents: [EKCalendar]? //also array of existing calendars (can this be removed?)
     
-    @IBAction func runCode(_ sender: UIButton) {
+    
+    
+    
+    func pullEventInfo() {
+        
+        var timeSchedule: [Date] = []
         
         //formats date according to entry (will go after date selector is implemented)
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM/dd/yyy"
         
-        let startDay = dateFormatter.date(from: "08/24/2017")
-        let endDay = dateFormatter.date(from: "08/25/2017")
+        let searchStart = dateFormatter.date(from: "08/24/2017")
+        let searchEnd = dateFormatter.date(from: "08/25/2017")
         
         //accesses list of calendars in Event Store that contain events and stores them in an array of EKCalendars
-        self.calendar = eventStore.calendars(for: EKEntityType.event)
+        self.existingCalendarArray = eventStore.calendars(for: EKEntityType.event)
+        
         
         //creates predicate (query) for events within the date range provided
-        let conflictPredicate = eventStore.predicateForEvents(withStart: startDay!, end: endDay!, calendars: calendar)
+        let eventPullPredicate = eventStore.predicateForEvents(withStart: searchStart!, end: searchEnd!, calendars: existingCalendarArray)
         
         //accesses list of events matching predicate and stores them in an array of EKEvents
-        self.conflict = eventStore.events(matching: conflictPredicate)
-        
-        //if the calendar array has members, print the titles of the calendars
-        if let calendar = calendar {
-            for i in 0...(calendar.count-1) {
-                print (calendar[i].title)
-            }
+        //sorts list of pulled events by start date
+        self.conflictArray = eventStore.events(matching: eventPullPredicate).sorted{
+            (ev1: EKEvent, ev2: EKEvent) -> Bool in
+            return ev1.startDate.compare(ev2.startDate) == ComparisonResult.orderedAscending
         }
         
         //if the conflict array has members, print the titles of the calendars
-        if let conflict = conflict {
-            for i in 0...(conflict.count-1) {
-                print (conflict[i].title)
-                print (conflict[i].startDate)
-                print (conflict[i].endDate)
-                print ("Next Event")
+        if let conflictArray = conflictArray {
+            for i in 0...(conflictArray.count-1) {
+                timeSchedule.append(conflictArray[i].startDate)
+                timeSchedule.append(conflictArray[i].endDate)
             }
         }
-        
-        
+        print (timeSchedule)
     }
     
-    @IBAction func newEvent(_ sender: UIButton) {
+    func createNewEvent() {
         
         //formats date according to entry (will go after date selector is implemented)
         let dateFormatter = DateFormatter()
@@ -63,23 +64,37 @@ class ViewController: UIViewController {
         let endTime = dateFormatter.date(from: "08/24/2017 23:30")
         
         //accesses list of calendars and stores them as EKCalendar objects in an array
-        self.calendar = eventStore.calendars(for: EKEntityType.event)
+        self.calendarsForEvents = eventStore.calendars(for: EKEntityType.event)
         
         //defines properties of new event
         let newEvent = EKEvent(eventStore: eventStore)
-        if let calendar = calendar {
-            newEvent.calendar = calendar[0]
+        if let calendarsForEvents = calendarsForEvents {
+            newEvent.calendar = calendarsForEvents[0]
         }
         newEvent.title = "Test Event from xCode"
         newEvent.startDate = startTime!
         newEvent.endDate = endTime!
         
+        //error handling: tries to save event, in case of error, it will print error
         do {
             try eventStore.save(newEvent, span: .thisEvent, commit: true)
         } catch let err as NSError {
             print (err.description)
         }
+    }
+    
+    func testForConflict() {
         
+    }
+    
+    
+    
+    @IBAction func runCode(_ sender: UIButton) {
+        pullEventInfo()
+    }
+    
+    @IBAction func newEvent(_ sender: UIButton) {
+        createNewEvent()
     }
 
     override func viewDidLoad() {
@@ -92,8 +107,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-//test test
 }
 
 //issues: - events are being pulled correctly, but are returning in GMT not CDT.
-//issues: - events are being saved correctly, but are saving in CDT and not GMT. 
+//issues: - events are being saved correctly, but are saving in CDT and not GMT.
+
+//to do: find how to compare members of the array to see if they are all in order! 
+//reverse engineer that to find empty slots ?? 
